017d6fa0b6ed0b9981b5d2c3b4540908cb554579
Alfarizi0611 <Pundabia99@gmail.com>
2024-01-04 08:50:39 +0700

    first commit

--- pesankeretaPHP/assets/inc/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$dbuser = "root";
-$dbpass = "";
-$host = "localhost";
-$db = "pesankereta";
-$mysqli = new mysqli($host, $dbuser, $dbpass, $db);
--- pesankeretaPHP/assets/inc/footer.php
+++
@@ -1,4 +0,0 @@
-<div class="splash-footer">
-    &copy; 2020 - <?php echo date ('Y');?>
-    Pemesanan Tiket Kereta Online | Developed By Alfarizi
-</div>
